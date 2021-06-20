82547efc78fe4c7a37a09c7535207a67e5b9b277
694952 <694952@sait227377it.ACDM.DS.SAIT.CA>
2021-06-20 01:30:24 -0600

    Finished everything

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package servlets;
 
 import java.io.IOException;
@@ -12,7 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import models.User;
+import services.AccountService;
 /**
  *
  * @author 694952
@@ -22,16 +19,56 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+         HttpSession session = request.getSession();
          
-        HttpSession session = request.getSession();
+         if (request.getParameter("logout") != null) {
+             session.removeAttribute("username");
+             session.removeAttribute("password");
+             session.setAttribute("message", "You have succesfully logged out!");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+         }
+         
+         else if (session.getAttribute("username") != null) {
+             
+             response.sendRedirect("home");
+         }
+         else {
+             getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            
+         }
         
-        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        HttpSession session = request.getSession();
+        
+        
+        String username = (String) request.getParameter("username");
+        String password = (String) request.getParameter("password");
+        
+        AccountService as = new AccountService();
+        
+        User user = new User();
+        if (username == null || username == "" || password == null || password == "") {
+            session.setAttribute("message", "Invalid login");
+            response.sendRedirect("login");
+        }
+        else {
+                user = as.login(username, password);
+                if (user == null) {
+                    session.setAttribute("message", "Invalid login");
+                    response.sendRedirect("login");
+                }
+                else {
+                    session.setAttribute("username", username);
+                    session.setAttribute("password", password);
+                    response.sendRedirect("home");
+                }
+            
+        }
     }
 
 
